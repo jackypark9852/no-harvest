666af11637e2fd95cb7a670c937ed13fee4bd1c0
Jacky Park <jackypark9852@gmail.com>
2023-01-07 01:40:12 -0600

    Implement TileUtil functions

--- Assets/Scripts/Tile/TileUtil.cs
+++ Assets/Scripts/Tile/TileUtil.cs
@@ -1,18 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Rendering;
 using UnityEngine;
 
-public class TileUtil : MonoBehaviour
+public static class TileUtil
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    static Grid grid;
 
-    // Update is called once per frame
-    void Update()
+    [RuntimeInitializeOnLoadMethod]
+    static void OnRuntimeMethodLoad()
     {
-        
+        Debug.Log("After Scene is loaded and game is running");
+        grid = GameObject.Find("Grid").GetComponent<Grid>();
+        Debug.Log(grid);
+    }
+    public static List<Tile> GetEmptyTiles()
+    {
+        Dictionary<Vector2Int, Tile> tiles = grid.Tiles;
+        List<Tile> emptyTiles = new List<Tile>();
+        foreach (KeyValuePair<Vector2Int, Tile> entry in tiles)
+        {
+            // Get the Tile object from the value of the KeyValuePair
+            Tile tile = entry.Value;
+
+            // Check if the plant property is null
+            if (tile.plant == null)
+            {
+                // If it is null, add the key (coordinate) to the list of empty tile coordinates
+                emptyTiles.Add(tile);
+            }
+        }
+
+        return emptyTiles;
+    }
+    public static List<Vector2Int> GetEmptyTileCoordinates()
+    {
+        Dictionary<Vector2Int, Tile> tiles = grid.Tiles;
+        List<Vector2Int> emptyTileCoordinates = new List<Vector2Int>();
+        foreach (KeyValuePair<Vector2Int, Tile> entry in tiles)
+        {
+            // Get the Tile object from the value of the KeyValuePair
+            Tile tile = entry.Value;
+
+            // Check if the plant property is null
+            if (tile.plant == null)
+            {
+                // If it is null, add the key (coordinate) to the list of empty tile coordinates
+                emptyTileCoordinates.Add(entry.Key);
+            }
+        }
+
+        return emptyTileCoordinates;
+    }
+    public static List<Tile> GetAffectedTiles(Vector2Int centerCoordinate, ShapeData shapeData)
+    {
+        Dictionary<Vector2Int, Tile> tiles = grid.Tiles;
+        List<Tile> validAffectedTiles = new List<Tile>();
+        Vector2Int[] affectedTiles = shapeData.affectedTiles;
+        foreach (Vector2Int affectedTile in affectedTiles)
+        {
+            Vector2Int tileCoordinate = centerCoordinate + affectedTile;
+            if (tiles.ContainsKey(tileCoordinate))
+            {
+                validAffectedTiles.Add(tiles[tileCoordinate]);
+            }
+        }
+        return validAffectedTiles;
     }
 }
