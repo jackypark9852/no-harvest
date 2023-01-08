8e52b9bb608486c1379fa0e8f40545aeedc16e38
Roger Wang <rogerwang2520@gmail.com>
2023-01-08 02:00:04 -0800

    Update game over score text

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameOver : MonoBehaviour
 {
     [SerializeField] GameObject gameOverMenu;
-
+    [SerializeField] TMP_Text scoreText;
+    
     public void StartGameOver()
     {
         gameOverMenu.SetActive(true);
+        scoreText.text = $"You said no to {GameManager.Instance.roundNum} days of harvest";
     }
 }
