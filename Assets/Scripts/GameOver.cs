f7b06e2fe1f6ab844654267dce65831333038713
Roger Wang <rogerwang2520@gmail.com>
2023-01-14 00:28:28 -0800

    Add score text to game over menu

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -7,10 +7,12 @@ public class GameOver : MonoBehaviour
 {
     [SerializeField] GameObject gameOverMenu;
     [SerializeField] TMP_Text scoreText;
+    [SerializeField] TMP_Text scoreText2;
     
     public void StartGameOver()
     {
         gameOverMenu.SetActive(true);
         scoreText.text = $"You said no to {RoundManager.Instance.roundNum} days of harvest";
+        scoreText2.text = $"Score: {ScoreManager.Instance.score}";
     }
 }
