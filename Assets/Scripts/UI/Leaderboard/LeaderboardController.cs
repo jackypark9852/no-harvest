294f38d5e0da5fc839d6d8c86971b3e9941fb24d
Jacky Park <jackypark9852@gmail.com>
2023-01-20 15:56:39 -0600

    Implement populating scrollable leaderboard with ScoreRows

--- Assets/Scripts/UI/Leaderboard/LeaderboardController.cs
+++ Assets/Scripts/UI/Leaderboard/LeaderboardController.cs
@@ -8,7 +8,9 @@ public class LeaderboardController : MonoBehaviour
 {
     [SerializeField] string leaderboardKey = "player_leaderboard"; // This is the leaderboard ID from the LootLockers's dashboard
     [SerializeField] TMPro.TMP_InputField playerIDInputField; 
-    [SerializeField] List<ScoreRow> scoreRows; 
+    [SerializeField] GameObject scoreRowsParent;
+    [SerializeField] GameObject scoreRowsPrefab;
+    [SerializeField] int maxRows = 500;
     [SerializeField] GameObject submitScorePanel;
     [SerializeField] GameObject leaderBoardPanel;
     int score = 0; 
@@ -47,24 +49,20 @@ public class LeaderboardController : MonoBehaviour
     public void DisplayScores() {
         submitScorePanel.SetActive(false);
         leaderBoardPanel.SetActive(true);
-        DisplayTopScores(scoreRows.GetRange(0, scoreRows.Count - 1));
-        DisplayPlayerScore(scoreRows[scoreRows.Count - 1]); 
-        
+        DisplayTopScores();
     }
 
-    public void DisplayTopScores(List<ScoreRow> scoreRows) {
-        LootLockerSDKManager.GetScoreList(leaderboardKey, scoreRows.Count, (response) =>
+    public void DisplayTopScores() {
+        LootLockerSDKManager.GetScoreList(leaderboardKey, maxRows, (response) =>
         {
             if (response.statusCode == 200)
             {
                 LootLockerLeaderboardMember[] scores = response.items;
 
-                for (int i = 0; i < scoreRows.Count; i++) 
+                for (int i = 0; i < scores.Length; i++) 
                 {
-                    ScoreRow scoreRow = scoreRows[i]; 
-                    scoreRow.rank.text  = string.Format("{0}.", i + 1); 
-                    scoreRow.name.text = scores[i].member_id;
-                    scoreRow.score.text = string.Format("{0}", scores[i].score);
+                    GameObject scoreRow = Object.Instantiate(scoreRowsPrefab, scoreRowsParent.transform);
+                    scoreRow.GetComponent<ScoreRow>().SetData(scores[i].rank, scores[i].member_id, scores[i].score);
                 }
             }
             else
@@ -73,32 +71,4 @@ public class LeaderboardController : MonoBehaviour
             }
         });
     }
-
-    public void DisplayPlayerScore(ScoreRow scoreRow) {
-        if(member_id != null) {
-            // This is to get the rank of the player
-            LootLockerSDKManager.GetMemberRank(leaderboardKey, member_id, (response) =>
-            {
-                if (response.statusCode == 200) {
-                    scoreRow.rank.text = string.Format("{0}.", response.rank);
-                    scoreRow.name.text = member_id;
-                    scoreRow.score.text = string.Format("{0}", response.score);
-                } else {
-                    Debug.LogError("Failed to retrieve player rank: " + response.Error);
-                }
-            });
-        } else { // If the player hasn't submitted a score yet
-            scoreRow.rank.text = "";
-            scoreRow.name.text = "";
-            scoreRow.score.text = "";
-        }
-    }
-}
-
-
-[System.Serializable]
-public struct ScoreRow {
-    public TMPro.TMP_Text rank;
-    public TMPro.TMP_Text name;
-    public TMPro.TMP_Text score;
 }
