c0b6aadbc8f3fd13d63f2aa2624b1d98117f2c9e
Jacky Park <jackypark9852@gmail.com>
2023-01-24 18:41:35 -0600

    Update leaderboard to allow duplicate playerIDs

--- Assets/Scripts/UI/Leaderboard/LeaderboardController.cs
+++ Assets/Scripts/UI/Leaderboard/LeaderboardController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using LootLocker.Requests;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,12 +9,11 @@ using UnityEngine.UI;
 public class LeaderboardController : MonoBehaviour
 {
     [Header("LootLocker API Settings")] 
+    [Tooltip("Leaderboard key from the LootLockers's dashboard")]
     [SerializeField] private List<Tuple<leaderboardType, string>> _keys = new List<Tuple<leaderboardType, string>>(); // This is the leaderboard ID from the LootLockers's dashboard
     private Dictionary<leaderboardType, string> _keysDict = new Dictionary<leaderboardType, string>();
 
     [Header("Score Display Settings")]
-    [SerializeField] private int _dayScoreMultiplier = 1000000; // The multiplier for the day score
-    [SerializeField] private int _scoreDayMultiplier = 100; // The multiplier for the score day
     [SerializeField] private int _maxRows = 1000;
     [SerializeField] private float _scrollYoffset = 0.7f; // The offset of the scroll rect from the player's score
     [SerializeField] private float _scrollDelaysSeconds = 0.5f; // The delay before scrolling to the player's score
@@ -33,22 +33,27 @@ public class LeaderboardController : MonoBehaviour
     [SerializeField] private ScrollRect _scrollRect; 
 
     // cached values, used to scroll to the player's score
-    private string _playerID = null; 
-    private int _playerScore = -1;  
-    private int _playerDay = -1; // The number of days survived by the player
+    private string _submissionID = null;
     private Dictionary<leaderboardType,string> _scoresTextDict = new Dictionary<leaderboardType, string>();
     private Dictionary<leaderboardType, int> _playerRankDict = new Dictionary<leaderboardType, int>(); 
+    private Dictionary<leaderboardType, bool> _submissionDoneFlags = new Dictionary<leaderboardType, bool>(); // The column that the leaderboard is sorted by
     private Coroutine _scrollCoroutine; 
-    
-    void Start()
-    {
-        LootLockerSDKManager.StartGuestSession((response) => 
+    private bool _sdkInitialized = false;
+    private void Awake() {
+        // Initialize the SDK
+        LootLockerSDKManager.StartGuestSession((response) =>  
         {
-            if (!response.success)
+            if (response.success)
             {
-                return;
+                _sdkInitialized = true;
+            } else {
+                Debug.LogError("failed: " + response.Error);
+                return; 
             }
         });
+    }
+    void Start()
+    {
         initializeLeaderboardKeyDict();
     }
     void initializeLeaderboardKeyDict() {
@@ -58,32 +63,41 @@ public class LeaderboardController : MonoBehaviour
     }
     // Submits score, days, and game result to leaderboards
     public void SubmitScore()
-    {
-        _playerID = _playerIDInputField.text;
-        if(_playerID == "") { // Hardcoded for now
-            // generate a random uid for playerID limited to 12 characters 
-            _playerID = System.Guid.NewGuid().ToString().Substring(0,8);
+    {   
+        if(!_sdkInitialized) {
+            Debug.LogWarning("SDK not initialized");
+            return;
         }
-        _playerScore = ScoreManager.Instance.score; 
-        _playerDay = RoundManager.Instance.roundNum;
 
-        int day_score = _playerDay*_dayScoreMultiplier + _playerScore; 
-        LootLockerSDKManager.SubmitScore(_playerID, day_score, _keysDict[leaderboardType.DayScore], (System.Action<LootLockerSubmitScoreResponse>)((response) =>
+        _submissionID = System.Guid.NewGuid().ToString(); // Generate a unique ID for the submission 
+
+        LeaderboardMetadata metadata = new LeaderboardMetadata();
+        metadata.PlayerID = _playerIDInputField.text; 
+        metadata.Score = ScoreManager.Instance.score;
+        metadata.Day = RoundManager.Instance.roundNum;
+        metadata.SubmissionID = _submissionID;
+        metadata.SubmissionTime = System.DateTime.Now.ToString();
+        string metaDataJSON = JsonUtility.ToJson(metadata);  // Convert the metadata to JSON string
+
+        _submissionDoneFlags[leaderboardType.DayScore] = false; // Submission started
+        LootLockerSDKManager.SubmitScore(metadata.SubmissionID, metadata.Day,  _keysDict[leaderboardType.DayScore], metaDataJSON, (System.Action<LootLockerSubmitScoreResponse>)((response) =>
         {
             if (response.statusCode == 200) {
                 _playerRankDict[leaderboardType.DayScore] = response.rank; // Cache the player's rank in the leaderboard
+                _submissionDoneFlags[leaderboardType.DayScore] = true; // Submission done
             } else {
-                Debug.Log("failed: " + response.Error);
+                Debug.LogError("failed: " + response.Error);
             }
         }));
 
-        int score_day = _playerScore*_scoreDayMultiplier + _playerDay; 
-        LootLockerSDKManager.SubmitScore(_playerID, score_day, _keysDict[leaderboardType.ScoreDay], (System.Action<LootLockerSubmitScoreResponse>)((response) =>
+        _submissionDoneFlags[leaderboardType.ScoreDay] = false; // Submission started
+        LootLockerSDKManager.SubmitScore(metadata.SubmissionID, metadata.Score, _keysDict[leaderboardType.ScoreDay], metaDataJSON, (System.Action<LootLockerSubmitScoreResponse>)((response) =>
         {
             if (response.statusCode == 200) {
                 _playerRankDict[leaderboardType.ScoreDay] = response.rank; // Cache the player's rank in the leaderboard
+                _submissionDoneFlags[leaderboardType.ScoreDay] = true; // Submission done
             } else {
-                Debug.Log("failed: " + response.Error);
+                Debug.LogError("failed: " + response.Error);
             }
         }));
     }
@@ -94,27 +108,32 @@ public class LeaderboardController : MonoBehaviour
     }
 
     public void DisplayScoreDay() {
-        Display(leaderboardType.ScoreDay);
+        StartCoroutine(Display(leaderboardType.ScoreDay));
     }
 
     public void DisplayDayScore() {
-        Display(leaderboardType.DayScore);
+        StartCoroutine(Display(leaderboardType.DayScore));
     }
 
-    void Display(leaderboardType type) {
+    IEnumerator Display(leaderboardType type) {
         if(_scrollCoroutine != null) {
             StopCoroutine(_scrollCoroutine); // Stop scrolling to player's score if it's still scrolling
         }
 
         if(_scoresTextDict.ContainsKey(type)) { // If the leaderboard text is already cached, display it
             _scoresText.text = _scoresTextDict[type];
-            return;
+            yield break;
+        }
+
+        // Check if all submissions are done, if not wait until they are before retrieving the leaderboard
+        while(!_submissionDoneFlags.All(x => x.Value)) {
+            yield return null;
         }
 
         LootLockerSDKManager.GetScoreList(_keysDict[type], _maxRows, (System.Action<LootLockerGetScoreListResponse>)((response) =>
         {
             if (response.statusCode == 200)
-            {
+            {; 
                 LootLockerLeaderboardMember[] scores = response.items;
                 _scoresText.text = GenerateText(type, scores);
                 _scoresTextDict[type] = _scoresText.text;
@@ -125,39 +144,41 @@ public class LeaderboardController : MonoBehaviour
                 Debug.LogError("Failed to retrieve leaderboard");
             }
         }));
+        yield break; 
     }
     // This method will generate the leaderboard text
     string GenerateText(leaderboardType type, LootLockerLeaderboardMember[] scores) {
-        string colorHex = ColorUtility.ToHtmlStringRGB(_columnLabelColor);
+        string columnHex = ColorUtility.ToHtmlStringRGB(_columnLabelColor);
         string sortColumnColorHex = ColorUtility.ToHtmlStringRGB(_sortColumnLabelColor);
         string leaderboardText;
         switch (type)
         {
             case leaderboardType.DayScore:
-                leaderboardText = string.Format("<color=#{0}> RANK  PLAYER ID     SCORE  <color=#{1}>DAY</color>\n</color>", colorHex, sortColumnColorHex);
+                leaderboardText = string.Format("<color=#{0}> RANK  PLAYER ID     SCORE  <color=#{1}>DAY</color>\n</color>", columnHex, sortColumnColorHex);
                 break;
             case leaderboardType.ScoreDay:
-                leaderboardText = string.Format("<color=#{0}> RANK  PLAYER ID     <color=#{1}>SCORE</color>  DAY\n</color>", colorHex, sortColumnColorHex);
+                leaderboardText = string.Format("<color=#{0}> RANK  PLAYER ID     <color=#{1}>SCORE</color>  DAY\n</color>", columnHex, sortColumnColorHex);
                 break;
             default:
-                leaderboardText = string.Format("<color=#{0}> RANK  PLAYER ID     SCORE  DAY\n</color>", colorHex); // Hardcoded for now
+                leaderboardText = string.Format("<color=#{0}> RANK  PLAYER ID     SCORE  DAY\n</color>", columnHex); // Hardcoded for now
                 break;
         }
         for (int i = 0; i < scores.Length; i++) 
         {
+            LeaderboardMetadata metadata = JsonUtility.FromJson<LeaderboardMetadata>(scores[i].metadata); // Convert the metadata string to a LeaderboardMetadata object
             string newText = ""; 
             switch(type) {
                 case leaderboardType.DayScore:
-                    newText = string.Format("{0,4}.  {1,-13}{3,6}  {2,3}\n", scores[i].rank, scores[i].member_id, scores[i].score/_dayScoreMultiplier, scores[i].score%_dayScoreMultiplier);
+                    newText = string.Format("{0,4}.  {1,-13}{3,6}  {2,3}\n", scores[i].rank, metadata.PlayerID, metadata.Score, metadata.Day);
                     break;
                 case leaderboardType.ScoreDay:
-                    newText = string.Format("{0,4}.  {1,-13}{3,6}  {2,3}\n", scores[i].rank, scores[i].member_id, scores[i].score%_scoreDayMultiplier, scores[i].score/_scoreDayMultiplier);
+                    newText = string.Format("{0,4}.  {1,-13}{3,6}  {2,3}\n", scores[i].rank, metadata.PlayerID, metadata.Score, metadata.Day);
                     break;
                 default:
                     throw new System.Exception("Leaderboard type not set");
             }
             
-            if (scores[i].member_id == _playerID) {
+            if (metadata.SubmissionID == _submissionID) { 
                 newText = string.Format("<color=#{0}>{1}</color>", ColorUtility.ToHtmlStringRGB(_playerScoreColor) ,newText); // Highlight the player's score
             }
             leaderboardText += newText;
@@ -173,7 +194,7 @@ public class LeaderboardController : MonoBehaviour
         float targetVerticalScrollPosition = CalculateVerticalScrollPosition(playerRank, rankCount);
         
         while(Mathf.Abs(leaderboardScrollRect.verticalNormalizedPosition - targetVerticalScrollPosition) > 0.01f) {
-            leaderboardScrollRect.verticalNormalizedPosition = Mathf.SmoothStep(leaderboardScrollRect.verticalNormalizedPosition, targetVerticalScrollPosition, _scrollSpeed);
+            leaderboardScrollRect.verticalNormalizedPosition = Mathf.Lerp(leaderboardScrollRect.verticalNormalizedPosition, targetVerticalScrollPosition, _scrollSpeed);
             yield return new WaitForEndOfFrame();
         }
         _scrollCoroutine = null; 
@@ -195,4 +216,13 @@ public enum leaderboardType{
     DayScore,
     ScoreDay,
     Uninitialized,
+}
+
+// This is the metadata that will be stored in the LootLocker leaderboard
+public struct LeaderboardMetadata { 
+    public string SubmissionID;
+    public string SubmissionTime; 
+    public string PlayerID;
+    public int Day;
+    public int Score;
 }
